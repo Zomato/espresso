51eae0a417f5c429f217847acf2aec6fc473889a
Akkahshh <akkahshh24@gmail.com>
2025-05-14 00:17:12 +0530

    Take config options from env

--- service/internal/config/config.go
+++ service/internal/config/config.go
@@ -1,19 +1,26 @@
 package config
 
 import (
+	"fmt"
+
 	"github.com/Zomato/espresso/service/model"
 	"github.com/spf13/viper"
 )
 
-func Load(filepath string) (model.Config, error) {
-	viper.SetConfigName("espressoconfig") // File name without extension
-	viper.SetConfigType("yaml")           // File type
-	// Search paths relative to where the binary runs in container
-	viper.AddConfigPath(filepath)        // Main config path in container
-	viper.AddConfigPath("../../configs") // For local development
-	viper.AddConfigPath("./configs")     // Fallback path for local development
-
+func Load() (model.Config, error) {
 	viper.AutomaticEnv()
+
+	viper.SetDefault(CONFIG_FILE_NAME, "espressoconfig")
+	viper.SetDefault(CONFIG_FILE_TYPE, "yaml")
+	viper.SetDefault(CONFIG_FILE_PATH, "/app/espresso/configs")
+
+	viper.SetConfigName(viper.GetString(CONFIG_FILE_NAME)) // File name without extension
+	viper.SetConfigType(viper.GetString(CONFIG_FILE_TYPE)) // File type
+
+	// Search paths relative to where the binary runs in container
+	viper.AddConfigPath(CONFIG_FILE_PATH) // Main config path in container
+	viper.AddConfigPath("../../configs")  // For local development
+	viper.AddConfigPath("./configs")      // Fallback path for local development
 
 	err := viper.ReadInConfig()
 	if err != nil {
@@ -27,8 +34,11 @@ func Load(filepath string) (model.Config, error) {
 	}
 
 	// TODO: Why are these fields set in the Dockerfile and not in the config.yaml file?
-	config.AppConfig.EnableUI = viper.GetBool("ENABLE_UI")
-	config.AppConfig.RodBrowserBin = viper.GetString("ROD_BROWSER_BIN")
+	config.AppConfig.EnableUI = viper.GetBool(ENABLE_UI)
+	config.AppConfig.RodBrowserBin = viper.GetString(ROD_BROWSER_BIN)
+	if config.AppConfig.RodBrowserBin == "" {
+		return model.Config{}, fmt.Errorf("environment variable %s not set", ROD_BROWSER_BIN)
+	}
 
 	return config, nil
 }
